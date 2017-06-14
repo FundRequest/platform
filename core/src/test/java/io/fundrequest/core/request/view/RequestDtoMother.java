b0d88ab39230c1c71d59619b9f99c96fce9c6dee
davyvanroy <davy.van.roy@gmail.com>
2017-06-14 20:29:50 +0200

    add issue information when retrieving by id

--- core/src/test/java/io/fundrequest/core/request/view/RequestDtoMother.java
+++ core/src/test/java/io/fundrequest/core/request/view/RequestDtoMother.java
@@ -1,14 +1,24 @@
 package io.fundrequest.core.request.view;
 
+import io.fundrequest.core.request.IssueInformationDtoMapperImpl;
 import io.fundrequest.core.request.RequestDtoMapper;
+import io.fundrequest.core.request.RequestDtoMapperImpl;
 import io.fundrequest.core.request.domain.RequestMother;
-import org.mapstruct.factory.Mappers;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.UUID;
 
 public final class RequestDtoMother {
 
-    private static RequestDtoMapper mapper = Mappers.getMapper(RequestDtoMapper.class);
+    private final static RequestDtoMapper mapper;
+
+    static {
+        mapper = new RequestDtoMapperImpl();
+        Field issueInformationDtoMapper = ReflectionUtils.findField(RequestDtoMapperImpl.class, "issueInformationDtoMapper");
+        ReflectionUtils.makeAccessible(issueInformationDtoMapper);
+        ReflectionUtils.setField(issueInformationDtoMapper, mapper, new IssueInformationDtoMapperImpl());
+    }
 
     public static RequestDto freeCodeCampNoUserStories() {
         return mapper.map(RequestMother.freeCodeCampNoUserStories()
