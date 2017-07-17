6102e2e903097b0d78e416371fb4b836a0d326b4
davyvanroy <davy.van.roy@gmail.com>
2017-07-17 22:53:14 +0200

    add angle

--- web/src/main/resources/static/js/common.js
+++ web/src/main/resources/static/js/common.js
@@ -3,6 +3,13 @@ requirejs.config({
     "paths": {
         "app": "../app",
         'jquery': '//code.jquery.com/jquery-3.2.1.min',
+        'jquery-storage-api': '//cdnjs.cloudflare.com/ajax/libs/jquery-storage-api/1.9.4/jquery.storageapi.min',
+        'jquery-easing': '//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min',
+        'jQuery-slimScroll': '//cdnjs.cloudflare.com/ajax/libs/jQuery-slimScroll/1.3.8/jquery.slimscroll.min',
+        'screenfull': '//cdnjs.cloudflare.com/ajax/libs/screenfull.js/3.3.1/screenfull.min',
+        'animo': '//cdnjs.cloudflare.com/ajax/libs/animo.js/1.0.3/animo.min',
+        'modernizr': 'modernizr.custom',
+        'angle': 'angle',
         'bootstrap': '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min',
 
         //DataTables core
@@ -12,18 +19,15 @@ requirejs.config({
     },
     "shim": {
 
-        'jquery': {
-            'exports': 'jquery'
-        },
-        'bootstrap': {
-            'deps': ['jquery'],
-            'exports': 'bootstrap'
-        },
-        'datatables.net': {
-            'deps': ['jquery', 'bootstrap']
-        },
-        'datatables.net-bs': {
-            'deps': ['jquery', 'bootstrap', 'datatables.net']
-        }
+        'jquery': {'exports': 'jquery'},
+        'bootstrap': {'deps': ['jquery'], 'exports': 'bootstrap'},
+        'datatables.net': {'deps': ['jquery', 'bootstrap']},
+        'datatables.net-bs': {'deps': ['jquery', 'bootstrap', 'datatables.net']},
+        'jquery-storage-api': {'deps': ['jquery']},
+        'jquery-easing': {'deps': ['jquery']},
+        'jQuery-slimScroll': {'deps': ['jquery']},
+        'screenfull': {'deps': ['jquery']},
+        'animo': {'deps': ['jquery']},
+        'angle': {'deps': ['jquery', 'modernizr', 'bootstrap', 'jquery-storage-api', 'jquery-easing', 'animo', 'jQuery-slimScroll', 'screenfull']}
     }
 });
