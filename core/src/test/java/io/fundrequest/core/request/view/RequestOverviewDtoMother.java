901ce6772654ad3b76944a503475d29dfdb221fe
davyvanroy <davy.van.roy@gmail.com>
2017-07-24 14:30:57 +0200

    update test

--- core/src/test/java/io/fundrequest/core/request/view/RequestOverviewDtoMother.java
+++ core/src/test/java/io/fundrequest/core/request/view/RequestOverviewDtoMother.java
@@ -1,13 +1,25 @@
 package io.fundrequest.core.request.view;
 
+import io.fundrequest.core.request.IssueInformationDtoMapperImpl;
+import io.fundrequest.core.request.RequestDtoMapperImpl;
 import io.fundrequest.core.request.RequestOverviewDtoMapper;
+import io.fundrequest.core.request.RequestOverviewDtoMapperImpl;
 import io.fundrequest.core.request.domain.RequestMother;
 import org.mapstruct.factory.Mappers;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.UUID;
 
 public final class RequestOverviewDtoMother {
-    private static RequestOverviewDtoMapper mapper = Mappers.getMapper(RequestOverviewDtoMapper.class);
+    private static RequestOverviewDtoMapper mapper;
+
+    static {
+        mapper = new RequestOverviewDtoMapperImpl();
+        Field issueInformationDtoMapper = ReflectionUtils.findField(RequestOverviewDtoMapperImpl.class, "issueInformationDtoMapper");
+        ReflectionUtils.makeAccessible(issueInformationDtoMapper);
+        ReflectionUtils.setField(issueInformationDtoMapper, mapper, new IssueInformationDtoMapperImpl());
+    }
 
     public static RequestOverviewDto freeCodeCampNoUserStories() {
         return mapper.map(RequestMother.freeCodeCampNoUserStories()
