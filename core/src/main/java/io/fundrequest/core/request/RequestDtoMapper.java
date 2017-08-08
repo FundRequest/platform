8cf06f134d43670b1a7375819a7a9ab7274283ed
Steve Maris <steve_maris@hotmail.com>
2017-08-08 23:27:59 +0200

    Remove RequestOverview classes, since it’s the same as the Request classes

--- core/src/main/java/io/fundrequest/core/request/RequestDtoMapper.java
+++ core/src/main/java/io/fundrequest/core/request/RequestDtoMapper.java
@@ -3,10 +3,15 @@ package io.fundrequest.core.request;
 import io.fundrequest.core.infrastructure.mapping.BaseMapper;
 import io.fundrequest.core.infrastructure.mapping.DefaultMappingConfig;
 import io.fundrequest.core.request.domain.Request;
+import io.fundrequest.core.request.mapper.RequestDtoMapperDecorator;
 import io.fundrequest.core.request.view.RequestDto;
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 @Mapper(config = DefaultMappingConfig.class,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
         uses = {IssueInformationDtoMapper.class})
+@DecoratedWith(RequestDtoMapperDecorator.class)
 public interface RequestDtoMapper extends BaseMapper<Request, RequestDto> {
 }
