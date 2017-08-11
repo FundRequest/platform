be343ce2d272c15af3ef074751b255bdcf19edcb
Steve Maris <steve_maris@hotmail.com>
2017-08-11 10:04:21 +0200

    Remove specific jquery, move to generic functions

--- web/src/main/resources/static/js/app/common/all-pages.js
+++ web/src/main/resources/static/js/app/common/all-pages.js
@@ -1,4 +1,6 @@
-define(function (require) {
+define(function(require) {
+    'use strict';
+
     var $ = require('jquery'),
         $document = $(document),
         datatables = require('datatables.net-bs');
@@ -19,4 +21,65 @@ define(function (require) {
                     .addClass('fa-star' + (watching ? '-o' : ''));
             });
     });
-});
+
+    // submit form and reload page after success
+    $document.on('fnd.form.submit', function(event, data) {
+        event.preventDefault();
+
+        var $form = $(data.form);
+        var $content = $(data.content);
+
+        $.post($form.attr('action'), $form.serialize()).done(function(data, textStatus, xhr) {
+            if (xhr.status === 204) {
+                location.reload();
+            } else {
+                $document.trigger('fnd.content.refresh', {
+                    content: $content,
+                    html: data
+                });
+            }
+        }).fail(function(xhr, textStatus, error) {
+            console.log(error);
+        });
+    });
+
+    // refresh content with new html
+    $document.on('fnd.content.refresh', function(event, data) {
+        var $content = $(data.content);
+        $content.html(data.html);
+
+        var $tagsinput = $('[data-role="tagsinput"]', $content);
+
+        if ($tagsinput.length > 0) {
+            $tagsinput.tagsinput('refresh');
+        }
+
+        $('form', $content).on('submit', function() {
+            $document.trigger('fnd.form.submit', {
+                content: $content,
+                form: this
+            });
+        });
+    });
+
+    // open modal
+    $document.on('click', '[data-toggle="modal"]', function(e) {
+        var button = this;
+        var $modal = $('[data-role="modal-template"]');
+        var path = $(button).data('modal-path');
+
+        $modal.modal('show');
+
+        $.get(path).done(function(data) {
+            var $content = $('.modal-content', $modal);
+            $document.trigger('fnd.content.refresh', {
+                content: $content,
+                html: data
+            });
+
+        }).fail(function(xhr, textStatus, error) {
+            console.log(error);
+        });
+    });
+})
+;
