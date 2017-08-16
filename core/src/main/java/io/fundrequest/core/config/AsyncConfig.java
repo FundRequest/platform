0d08eb8296a3bf96177dcb11559840e7b4cd5e5c
davyvanroy <davy.van.roy@gmail.com>
2017-08-16 22:44:53 +0200

    remove unused config

--- core/src/main/java/io/fundrequest/core/config/AsyncConfig.java
+++
@@ -1,9 +0,0 @@
-package io.fundrequest.core.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@EnableAsync
-@Configuration
-public class AsyncConfig {
-}
