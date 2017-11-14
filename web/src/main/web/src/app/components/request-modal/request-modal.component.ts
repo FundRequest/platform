e695c81c74c58977b6de5bccb47d273f9f446fe7
Steve Maris <steve.maris@kbc.be>
2017-11-14 12:53:40 +0100

    https://github.com/FundRequest/area51/issues/8 : Add request : Check if there is already a request for the provided link.

--- web/src/main/web/src/app/components/request-modal/request-modal.component.ts
+++ web/src/main/web/src/app/components/request-modal/request-modal.component.ts
@@ -1,27 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BsModalRef} from "ngx-bootstrap";
 import {RequestService} from "../../services/request/request.service";
+import {IRequestList, IRequestRecord} from "../../redux/requests.models";
+import {Observable} from "rxjs/Observable";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'fnd-request-modal',
   templateUrl: './request-modal.component.html',
   styleUrls: ['./request-modal.component.scss']
 })
-export class RequestModalComponent {
+export class RequestModalComponent implements OnInit, OnDestroy {
+  private _requests: IRequestList;
+  private _subscription: Subscription;
   public title: string;
   public issueLink: string;
   public technologies: any = [];
 
-  constructor(public bsModalRef: BsModalRef, private requestService: RequestService) {
+  constructor(public bsModalRef: BsModalRef, private _rs: RequestService) {
+  }
+
+  private issueExists(): boolean {
+    if (this.issueLink != null && this._requests) {
+      return this._requests.filter((request: IRequestRecord) => request.issueInformation.link == this.issueLink.trim()).count() > 0;
+    }
+  }
+
+  ngOnInit() {
     this.title = 'Add Request';
+    this._subscription = this._rs.requests$.subscribe(result => this._requests = result);
+  }
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
   }
 
   public addRequest() {
     let technologies = [];
-    for(let i = 0; i < this.technologies.length; i++) {
+    for (let i = 0; i < this.technologies.length; i++) {
       technologies.push(this.technologies[i].value);
     }
-    this.requestService.addRequest(this.issueLink, technologies);
+    this._rs.addRequest(this.issueLink.trim(), technologies);
     this.bsModalRef.hide();
   }
 }
