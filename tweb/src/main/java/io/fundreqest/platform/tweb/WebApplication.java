17b44abb7ba34dec52e0f45be085f6df586fb475
davyvanroy <davy.van.roy@gmail.com>
2018-03-21 10:03:33 +0100

    add thymeleaf and application

---
+++ tweb/src/main/java/io/fundreqest/platform/tweb/WebApplication.java
@@ -0,0 +1,25 @@
+package io.fundreqest.platform.tweb;
+
+import io.fundrequest.core.FundRequestCore;
+import io.fundrequest.core.infrastructure.IgnoreDuringComponentScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan(
+        basePackageClasses = {WebApplication.class, FundRequestCore.class},
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class),
+                @ComponentScan.Filter(IgnoreDuringComponentScan.class)})
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
