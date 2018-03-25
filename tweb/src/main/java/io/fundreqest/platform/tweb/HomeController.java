9f2f031d99facf448cdbf101cd67d818e72c4363
davyvanroy <davy.van.roy@gmail.com>
2018-03-25 19:48:17 +0200

    add security config

--- tweb/src/main/java/io/fundreqest/platform/tweb/HomeController.java
+++ tweb/src/main/java/io/fundreqest/platform/tweb/HomeController.java
@@ -1,8 +1,12 @@
 package io.fundreqest.platform.tweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -10,5 +14,11 @@ public class HomeController {
     @RequestMapping("/")
     public ModelAndView home() {
         return new ModelAndView("index");
+    }
+
+    @GetMapping(path = "/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "redirect:/";
     }
 }
