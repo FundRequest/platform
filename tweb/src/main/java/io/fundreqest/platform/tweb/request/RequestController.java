4864fed5b317c2b766602512c768bade570333a4
davyvanroy <davy.van.roy@gmail.com>
2018-04-16 17:04:43 +0200

    add erc67 endpoint

--- tweb/src/main/java/io/fundreqest/platform/tweb/request/RequestController.java
+++ tweb/src/main/java/io/fundreqest/platform/tweb/request/RequestController.java
@@ -2,10 +2,16 @@ package io.fundreqest.platform.tweb.request;
 
 import io.fundreqest.platform.tweb.infrastructure.mav.AbstractController;
 import io.fundrequest.core.request.RequestService;
+import io.fundrequest.core.request.fund.CreateERC67FundRequest;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class RequestController extends AbstractController {
@@ -30,6 +36,11 @@ public class RequestController extends AbstractController {
                 .withObject("request", requestService.findRequest(id))
                 .withView("pages/requests/detail")
                 .build();
+    }
+
+    @PostMapping(value = {"/rest/requests/{id}/erc67/fund"}, produces = MediaType.TEXT_PLAIN_VALUE)
+    public String generateERC67ForFunding(@RequestBody @Valid CreateERC67FundRequest createERC67FundRequest) {
+        return requestService.generateERC67(createERC67FundRequest);
     }
 
 
