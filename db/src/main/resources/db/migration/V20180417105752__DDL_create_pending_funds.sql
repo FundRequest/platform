2414152825da58c9ad841f20bee167de59333d56
Quinten De Swaef <quintendeswaef@gmail.com>
2018-04-17 14:05:52 +0200

    DB for pending funds

---
+++ db/src/main/resources/db/migration/V20180417105752__DDL_create_pending_funds.sql
@@ -0,0 +1,11 @@
+create table pending_funds (
+  id               BIGINT PRIMARY KEY AUTO_INCREMENT,
+  transaction_hash varchar(66)        default null,
+  description      text               default null,
+  address_from     varchar(42)        default null,
+  amount           varchar(100)       default 0,
+  token_address    varchar(42)        default null,
+  time_submitted   timestamp          default now()
+);
+
+create unique index idx_tx_hash_pending_funds on pending_funds(transaction_hash);
