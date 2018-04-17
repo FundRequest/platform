4de8b07d3605e6d3ef97b19a5c1470e014369e30
davyvanroy <davy.van.roy@gmail.com>
2018-04-17 10:15:42 +0200

    fix endpoint erc67

--- tweb/src/main/java/io/fundreqest/platform/tweb/request/RequestController.java
+++ tweb/src/main/java/io/fundreqest/platform/tweb/request/RequestController.java
@@ -5,10 +5,7 @@ import io.fundrequest.core.request.RequestService;
 import io.fundrequest.core.request.fund.CreateERC67FundRequest;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -22,7 +19,7 @@ public class RequestController extends AbstractController {
         this.requestService = requestService;
     }
 
-    @RequestMapping("/requests")
+    @GetMapping("/requests")
     public ModelAndView requests() {
         return modelAndView()
                 .withObject("requests", requestService.findAll())
@@ -30,7 +27,7 @@ public class RequestController extends AbstractController {
                 .build();
     }
 
-    @RequestMapping("/requests/{id}")
+    @GetMapping("/requests/{id}")
     public ModelAndView details(@PathVariable Long id) {
         return modelAndView()
                 .withObject("request", requestService.findRequest(id))
@@ -38,7 +35,8 @@ public class RequestController extends AbstractController {
                 .build();
     }
 
-    @PostMapping(value = {"/rest/requests/erc67/fund"}, produces = MediaType.TEXT_PLAIN_VALUE)
+    @PostMapping(value = {"/rest/requests/erc67/fund"}, produces = MediaType.TEXT_PLAIN_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
     public String generateERC67ForFunding(@RequestBody @Valid CreateERC67FundRequest createERC67FundRequest) {
         return requestService.generateERC67(createERC67FundRequest);
     }
