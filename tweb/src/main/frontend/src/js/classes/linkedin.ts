199d31f4b4e9aa99b5347bf8eabfd1633223b3bd
Steve Maris <steve.maris@fundrequest.io>
2018-05-02 09:33:51 +0200

    Fix codeclimate: Function constructor has 32 lines of code (exceeds 25 allowed).

--- tweb/src/main/frontend/src/js/classes/linkedin.ts
+++ tweb/src/main/frontend/src/js/classes/linkedin.ts
@@ -1,5 +1,6 @@
 import {Alert} from './alert';
 import * as $ from 'jquery';
+import {Utils} from './Utils';
 
 interface LinkedInData {
     id: string;
@@ -17,47 +18,44 @@ class LinkedIn {
         let modal = document.querySelector('#modal-share-linked-in');
 
         if (button != null) {
-            button.addEventListener('click', (e) => {
-                $.post('/bounties/linkedin', {'post-id': this._postId}).promise()
-                    .then(() => {
-                        $(modal).modal('hide');
-                        Alert.show('Sharing is caring, thanks!');
-                    })
-                    .catch(() => {
-                        Alert.show('Oops, something went wrong, please try again.', 'danger');
-                    })
-            });
-
-            $(modal).on('show.bs.modal', (e) => {
-                let target: HTMLElement = (e.target as HTMLElement);
-                let title: HTMLElement = target.querySelector('[data-share-linked-in="title"]') as HTMLElement;
-                let message: HTMLElement = target.querySelector('[data-share-linked-in="text"]') as HTMLElement;
-                let image: HTMLImageElement = target.querySelector('[data-share-linked-in="image"]') as HTMLImageElement;
-                let url: HTMLAnchorElement = target.querySelector('[data-share-linked-in="url"]') as HTMLAnchorElement;
-
-                this._getLinkedInPost()
-                    .then((data: LinkedInData) => {
-                        this._postId = data.id;
-                        url ? url.href = data.submittedUrl : null;
-                        image ? image.src = data.submittedImageUrl : null;
-                        title ? title.innerHTML = data.title : null;
-                        message ? message.innerHTML = data.description : null;
-                    })
-                    .catch(function (ex) {
-                        console.log('Something when wrong during getting LinkedIn post', ex);
-                    });
-            });
+            this._addButtonEvents(button, '/bounties/linkedin', {'post-id': this._postId}, modal);
+            this._addModalEvents(modal);
         }
     }
 
-    private _getLinkedInPost(): Promise<any> {
-        return $.get('/bounties/linkedin/random-post').promise();
+    private _addButtonEvents(button, address, data, modal) {
+        button.addEventListener('click', (e) => {
+            Utils.fetchJSON(address, data)
+                .then(() => {
+                    $(modal).modal('hide');
+                    Alert.show('Sharing is caring, thanks!');
+                })
+                .catch(() => {
+                    Alert.show('Oops, something went wrong, please try again.', 'danger');
+                });
+        });
+    }
 
-        /*await fetch('/bounties/linkedin/random-post', {mode: 'no-cors'})
-            .then((response) => response.json())
-            .catch(function (ex) {
-                console.log('Something when wrong during getting LinkedIn post', ex);
-            }) as LinkedInData;*/
+    private _addModalEvents(modal) {
+        $(modal).on('show.bs.modal', (e) => {
+            let target: HTMLElement = (e.target as HTMLElement);
+            let title: HTMLElement = target.querySelector('[data-share-linked-in="title"]') as HTMLElement;
+            let message: HTMLElement = target.querySelector('[data-share-linked-in="text"]') as HTMLElement;
+            let image: HTMLImageElement = target.querySelector('[data-share-linked-in="image"]') as HTMLImageElement;
+            let url: HTMLAnchorElement = target.querySelector('[data-share-linked-in="url"]') as HTMLAnchorElement;
+
+            Utils.fetchJSON('/bounties/linkedin/random-post')
+                .then((data: LinkedInData) => {
+                    this._postId = data.id;
+                    url ? url.href = data.submittedUrl : null;
+                    image ? image.src = data.submittedImageUrl : null;
+                    title ? title.innerHTML = data.title : null;
+                    message ? message.innerHTML = data.description : null;
+                })
+                .catch(function (ex) {
+                    console.log('Something when wrong during getting LinkedIn post', ex);
+                });
+        });
     }
 }
 
