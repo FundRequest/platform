c3660d1b21ed4548ecb504a1607532eebe78a3a6
Nico Peeters <nico@itwhiz.be>
2018-05-07 10:45:38 +0200

    #160 - Fix github feign config + remove double escape in messages

--- github/src/main/java/io/fundrequest/platform/github/GitHubCommentFactory.java
+++ github/src/main/java/io/fundrequest/platform/github/GitHubCommentFactory.java
@@ -9,35 +9,35 @@ public class GitHubCommentFactory {
     private static final String FUNDED_COMMENT_TEMPLATE =
             "[![](%1$s/requests/%2$s/badge)](%1$s/requests/%2$s)"
             + " [![](%1$s/assets/img/powered-by-fundrequest-badge.svg)](https://fundrequest.io)"
-            + "<a href=\\\"https://fundrequest.io\\\"><img align=\\\"right\\\" src=\\\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\\\"></a>"
-            + "\\r\\n"
+            + "<a href=\"https://fundrequest.io\"><img align=\"right\" src=\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\"></a>"
+            + "\r\n"
             + "This issue has been funded using [FundRequest](https://fundrequest.io). A developer can claim the reward by submitting a pull request referencing this issue. "
             + "([How to Close Issues via Pull Requests?](https://help.github.com/articles/closing-issues-using-keywords)) e.g. `fixes #%2$s`"
-            + "\\r\\n"
-            + "\\r\\n* For more help on how to claim on issue, please visit our [help section](https://help.fundrequest.io)."
-            + "\\r\\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
-            + "\\r\\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
+            + "\r\n"
+            + "\r\n* For more help on how to claim on issue, please visit our [help section](https://help.fundrequest.io)."
+            + "\r\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
+            + "\r\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
 
     private static final String RESOLVED_COMMENT_TEMPLATE =
             "[![](%1$s/requests/%2$s/badge)](%1$s/requests/%2$s)"
             + " [![](%1$s/assets/img/powered-by-fundrequest-badge.svg)](https://fundrequest.io)"
-            + "<a href=\\\"https://fundrequest.io\\\"><img align=\\\"right\\\" src=\\\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\\\"></a>"
-            + "\\r\\n"
+            + "<a href=\"https://fundrequest.io\"><img align=\"right\" src=\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\"></a>"
+            + "\r\n"
             + "Thank you @%3$s for your code contribution. You can now claim the reward that is linked to this issue. This can be done via our "
             + "[Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest) or directly from the [funded request](%1$s/requests/%2$s)."
-            + "\\r\\n"
-            + "\\r\\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
-            + "\\r\\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
+            + "\r\n"
+            + "\r\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
+            + "\r\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
 
     private static final String CLOSED_COMMENT_TEMPLATE =
             "[![](%1$s/requests/%2$s/badge)](%1$s/requests/%2$s)"
             + " [![](%1$s/assets/img/powered-by-fundrequest-badge.svg)](https://fundrequest.io)"
-            + "<a href=\\\"https://fundrequest.io\\\"><img align=\\\"right\\\" src=\\\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\\\"></a>"
-            + "\\r\\n"
+            + "<a href=\"https://fundrequest.io\"><img align=\"right\" src=\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\"></a>"
+            + "\r\n"
             + "Thank you @%3$s for your code contribution. The reward linked to this issue has been transferred to your account."
-            + "\\r\\n"
-            + "\\r\\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
-            + "\\r\\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
+            + "\r\n"
+            + "\r\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
+            + "\r\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
 
     private final String badgeBasepath;
 
--- github/src/test/java/io/fundrequest/platform/github/GitHubCommentFactoryTest.java
+++ github/src/test/java/io/fundrequest/platform/github/GitHubCommentFactoryTest.java
@@ -10,35 +10,35 @@ public class GitHubCommentFactoryTest {
     private final static String EXPECTED_FUNDED_COMMENT =
             "[![](https://fundrequest.io/requests/156/badge)](https://fundrequest.io/requests/156)"
             + " [![](https://fundrequest.io/assets/img/powered-by-fundrequest-badge.svg)](https://fundrequest.io)"
-            + "<a href=\\\"https://fundrequest.io\\\"><img align=\\\"right\\\" src=\\\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\\\"></a>"
-            + "\\r\\n"
+            + "<a href=\"https://fundrequest.io\"><img align=\"right\" src=\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\"></a>"
+            + "\r\n"
             + "This issue has been funded using [FundRequest](https://fundrequest.io). A developer can claim the reward by submitting a pull request referencing this issue. "
             + "([How to Close Issues via Pull Requests?](https://help.github.com/articles/closing-issues-using-keywords)) e.g. `fixes #156`"
-            + "\\r\\n"
-            + "\\r\\n* For more help on how to claim on issue, please visit our [help section](https://help.fundrequest.io)."
-            + "\\r\\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
-            + "\\r\\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
+            + "\r\n"
+            + "\r\n* For more help on how to claim on issue, please visit our [help section](https://help.fundrequest.io)."
+            + "\r\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
+            + "\r\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
 
     private final static String EXPECTED_RESOLVED_COMMENT =
             "[![](https://fundrequest.io/requests/635/badge)](https://fundrequest.io/requests/635)"
             + " [![](https://fundrequest.io/assets/img/powered-by-fundrequest-badge.svg)](https://fundrequest.io)"
-            + "<a href=\\\"https://fundrequest.io\\\"><img align=\\\"right\\\" src=\\\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\\\"></a>"
-            + "\\r\\n"
+            + "<a href=\"https://fundrequest.io\"><img align=\"right\" src=\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\"></a>"
+            + "\r\n"
             + "Thank you @dfghd-ghjgfg for your code contribution. You can now claim the reward that is linked to this issue. This can be done via our "
             + "[Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest) or directly from the [funded request](https://fundrequest.io/requests/635)."
-            + "\\r\\n"
-            + "\\r\\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
-            + "\\r\\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
+            + "\r\n"
+            + "\r\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
+            + "\r\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
 
     private final static String EXPECTED_CLOSED_COMMENT =
             "[![](https://fundrequest.io/requests/5473/badge)](https://fundrequest.io/requests/5473)"
             + " [![](https://fundrequest.io/assets/img/powered-by-fundrequest-badge.svg)](https://fundrequest.io)"
-            + "<a href=\\\"https://fundrequest.io\\\"><img align=\\\"right\\\" src=\\\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\\\"></a>"
-            + "\\r\\n"
+            + "<a href=\"https://fundrequest.io\"><img align=\"right\" src=\"https://avatars0.githubusercontent.com/u/22447793?s=20&v=4\"></a>"
+            + "\r\n"
             + "Thank you @ljn-ytd for your code contribution. The reward linked to this issue has been transferred to your account."
-            + "\\r\\n"
-            + "\\r\\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
-            + "\\r\\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
+            + "\r\n"
+            + "\r\n* For better GitHub integration please install the FundRequest [Chome browser plugin](https://chrome.google.com/webstore/search/fundrequest)."
+            + "\r\n* Looking for more? Feel free to [browse](https://fundrequest.io/requests) through all funded requests.";
 
     private GitHubCommentFactory gitHubCommentFactory;
 
