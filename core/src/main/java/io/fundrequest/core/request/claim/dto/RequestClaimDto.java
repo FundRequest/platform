34fa57307e2dd6c474b05e622c2e4208e0093433
Nico Peeters <nico@itwhiz.be>
2018-05-08 20:47:28 +0200

    #212 - Revert unnecessary RequestClaimDto.java changes

--- core/src/main/java/io/fundrequest/core/request/claim/dto/RequestClaimDto.java
+++ core/src/main/java/io/fundrequest/core/request/claim/dto/RequestClaimDto.java
@@ -1,6 +1,5 @@
 package io.fundrequest.core.request.claim.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.fundrequest.core.request.claim.domain.ClaimRequestStatus;
 import lombok.Builder;
 import lombok.Data;
@@ -12,21 +11,20 @@ public class RequestClaimDto {
     private String solver;
     private ClaimRequestStatus status = ClaimRequestStatus.PENDING;
     private Boolean flagged;
-    @JsonProperty("request_id")
-    private Long requestId;
     private String url;
+
 
     RequestClaimDto() {
     }
 
+
     @Builder
-    public RequestClaimDto(Long id, String address, String solver, ClaimRequestStatus status, Boolean flagged, Long requestId, String url) {
+    public RequestClaimDto(Long id, String address, String solver, ClaimRequestStatus status, Boolean flagged, String url) {
         this.id = id;
         this.address = address;
         this.solver = solver;
         this.status = status;
         this.flagged = flagged;
-        this.requestId = requestId;
         this.url = url;
     }
 }
