d21869e96c356685dafc9c9e6b0ca7cec2fdf167
Nico Peeters <nico@itwhiz.be>
2018-05-08 14:31:35 +0200

    #212 - In the admin panel; link to the correct github issue

--- core/src/main/java/io/fundrequest/core/request/claim/dto/RequestClaimDto.java
+++ core/src/main/java/io/fundrequest/core/request/claim/dto/RequestClaimDto.java
@@ -1,5 +1,6 @@
 package io.fundrequest.core.request.claim.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.fundrequest.core.request.claim.domain.ClaimRequestStatus;
 import lombok.Builder;
 import lombok.Data;
@@ -11,20 +12,21 @@ public class RequestClaimDto {
     private String solver;
     private ClaimRequestStatus status = ClaimRequestStatus.PENDING;
     private Boolean flagged;
+    @JsonProperty("request_id")
+    private Long requestId;
     private String url;
-
 
     RequestClaimDto() {
     }
 
-
     @Builder
-    public RequestClaimDto(Long id, String address, String solver, ClaimRequestStatus status, Boolean flagged, String url) {
+    public RequestClaimDto(Long id, String address, String solver, ClaimRequestStatus status, Boolean flagged, Long requestId, String url) {
         this.id = id;
         this.address = address;
         this.solver = solver;
         this.status = status;
         this.flagged = flagged;
+        this.requestId = requestId;
         this.url = url;
     }
 }
