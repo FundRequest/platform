56e9b299a4a9b6e1354931d5d2f2418d711ff1b6
Quinten De Swaef <quintendeswaef@gmail.com>
2018-05-09 11:05:14 +0200

    #209 better transaction handling

--- core/src/main/java/io/fundrequest/core/request/fund/continuous/PendingFundCleaner.java
+++ core/src/main/java/io/fundrequest/core/request/fund/continuous/PendingFundCleaner.java
@@ -3,12 +3,14 @@ package io.fundrequest.core.request.fund.continuous;
 import io.fundrequest.core.request.fund.PendingFundService;
 import io.fundrequest.core.request.infrastructure.azrael.AzraelClient;
 import io.fundrequest.core.transactions.TransactionStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import static io.fundrequest.core.web3j.AddressUtils.prettify;
 
 @Component
+@Slf4j
 public class PendingFundCleaner {
 
     private final PendingFundService pendingFundService;
@@ -24,7 +26,14 @@ public class PendingFundCleaner {
     public void cleanupPendingFunds() {
         pendingFundService.findAll()
                           .stream()
-                          .filter(fund -> azraelClient.getTransactionStatus(prettify(fund.getTransactionHash())).equals(TransactionStatus.FAILED))
+                          .filter(fund -> {
+                              try {
+                                  return azraelClient.getTransactionStatus(prettify(fund.getTransactionHash())).equals(TransactionStatus.FAILED);
+                              } catch (final Exception ex) {
+                                  log.error("Unable to fetch status for transaction {} : ", fund.getTransactionHash(), ex.getMessage());
+                                  return false;
+                              }
+                          })
                           .forEach(pendingFundService::removePendingFund);
     }
 }
