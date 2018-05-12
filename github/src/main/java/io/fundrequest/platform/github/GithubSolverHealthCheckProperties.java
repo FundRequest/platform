52a8e9fe9bc545fabbd8460a619d3ed211688390
davyvanroy <davy.van.roy@gmail.com>
2018-05-12 21:35:55 +0200

    fix startup issues

--- github/src/main/java/io/fundrequest/platform/github/GithubSolverHealthCheckProperties.java
+++ github/src/main/java/io/fundrequest/platform/github/GithubSolverHealthCheckProperties.java
@@ -1,7 +1,9 @@
 package io.fundrequest.platform.github;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Map;
@@ -9,6 +11,8 @@ import java.util.Map;
 @Data
 @Builder
 @ConfigurationProperties("io.fundrequest.health.github.solver")
+@NoArgsConstructor
+@AllArgsConstructor
 public class GithubSolverHealthCheckProperties {
 
     private String owner;
