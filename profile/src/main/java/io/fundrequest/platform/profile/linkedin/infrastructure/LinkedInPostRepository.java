d515f1f742156f263ed1d41cbea548c2870b7617
davyvanroy <davy.van.roy@gmail.com>
2018-05-17 15:11:16 +0200

    remove linkedin

--- profile/src/main/java/io/fundrequest/platform/profile/linkedin/infrastructure/LinkedInPostRepository.java
+++
@@ -1,7 +0,0 @@
-package io.fundrequest.platform.profile.linkedin.infrastructure;
-
-import io.fundrequest.platform.profile.linkedin.domain.LinkedInPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LinkedInPostRepository extends JpaRepository<LinkedInPost, Long> {
-}
