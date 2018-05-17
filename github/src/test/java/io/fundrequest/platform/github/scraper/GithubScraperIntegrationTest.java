3812a36bfd2a07ae4551ac2e0b867de796d30d23
davyvanroy <davy.van.roy@gmail.com>
2018-05-17 16:01:06 +0200

    update properties

--- github/src/test/java/io/fundrequest/platform/github/scraper/GithubScraperIntegrationTest.java
+++ github/src/test/java/io/fundrequest/platform/github/scraper/GithubScraperIntegrationTest.java
@@ -6,17 +6,23 @@ import io.fundrequest.platform.github.scraper.model.GithubIssue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {FundRequestGithub.class })
+@TestPropertySource(locations="classpath:application-test.properties")
 public class GithubScraperIntegrationTest {
 
     @Autowired
     private GithubScraper scraper;
+
+    @Autowired
+    private Environment env;
 
     @Test
     public void fetch() {
