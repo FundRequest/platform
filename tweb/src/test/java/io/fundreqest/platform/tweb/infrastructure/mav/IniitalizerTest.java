661220be4bfd89f78a8cab6ee6cac2c1dcd23fc7
davyvanroy <davy.van.roy@gmail.com>
2018-05-23 16:34:58 +0200

    initialize cache

---
+++ tweb/src/test/java/io/fundreqest/platform/tweb/infrastructure/mav/IniitalizerTest.java
@@ -0,0 +1,40 @@
+package io.fundreqest.platform.tweb.infrastructure.mav;
+
+import io.fundrequest.core.request.RequestService;
+import io.fundrequest.core.request.statistics.StatisticsService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class IniitalizerTest {
+
+    private Iniitalizer iniitalizer;
+
+    @BeforeEach
+    void setUp() {
+        iniitalizer = new Iniitalizer();
+    }
+
+    @Test
+    void initializesCachedData() {
+        ContextRefreshedEvent event = mock(ContextRefreshedEvent.class);
+        ApplicationContext context = mock(ApplicationContext.class);
+        when(event.getApplicationContext()).thenReturn(context);
+        RequestService requestService = mock(RequestService.class);
+        when(context.getBean(RequestService.class)).thenReturn(requestService);
+        StatisticsService statisticsService = mock(StatisticsService.class);
+        when(context.getBean(StatisticsService.class)).thenReturn(statisticsService);
+
+        iniitalizer.onApplicationEvent(event);
+
+        verify(requestService).findAll();
+        verify(requestService).findAllTechnologies();
+        verify(requestService).findAllProjects();
+        verify(statisticsService).getStatistics();
+    }
+}
