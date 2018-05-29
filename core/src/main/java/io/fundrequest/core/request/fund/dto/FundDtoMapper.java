ebb29bc0d6f52407c2c624e02bb897ad11da1799
Nico Peeters <nico@itwhiz.be>
2018-05-29 16:17:55 +0200

    Merge branch 'release-boccaccio' into feature-268-change-funded-by-tab
    # Conflicts:
    #	admin-web/src/main/resources/application.properties
    #	core/src/main/java/io/fundrequest/core/request/RequestServiceImpl.java
    #	core/src/main/java/io/fundrequest/core/request/claim/domain/Claim.java
    #	core/src/main/java/io/fundrequest/core/request/claim/domain/ClaimBuilder.java
    #	core/src/main/java/io/fundrequest/core/request/claim/dto/ClaimDto.java
    #	core/src/main/java/io/fundrequest/core/request/claim/dto/ClaimDtoMapper.java
    #	core/src/main/java/io/fundrequest/core/request/fund/FundServiceImpl.java
    #	core/src/main/java/io/fundrequest/core/request/fund/domain/Fund.java
    #	core/src/main/java/io/fundrequest/core/request/fund/dto/FundDtoMapper.java
    #	core/src/test/java/io/fundrequest/core/request/RequestServiceImplTest.java
    #	core/src/test/java/io/fundrequest/core/request/fund/FundServiceImplTest.java
    #	core/src/test/java/io/fundrequest/core/request/view/FundDtoMother.java
    #	tweb/src/main/java/io/fundreqest/platform/tweb/request/messsaging/AzraelMessageReceiver.java
    #	tweb/src/test/java/io/fundrequest/AzraelMessageReceiverTest.java

--- core/src/main/java/io/fundrequest/core/request/fund/dto/FundDtoMapper.java
+++ core/src/main/java/io/fundrequest/core/request/fund/dto/FundDtoMapper.java
@@ -3,11 +3,10 @@ package io.fundrequest.core.request.fund.dto;
 import io.fundrequest.core.infrastructure.mapping.BaseMapper;
 import io.fundrequest.core.infrastructure.mapping.DefaultMappingConfig;
 import io.fundrequest.core.request.fund.domain.Fund;
-import io.fundrequest.core.request.mapper.BlockchainEventDtoMapper;
 import io.fundrequest.core.token.mapper.TokenValueDtoMapper;
 import org.mapstruct.Mapper;
 
 @Mapper(config = DefaultMappingConfig.class,
-        uses = {BlockchainEventDtoMapper.class, TokenValueDtoMapper.class})
+        uses = {TokenValueDtoMapper.class})
 public interface FundDtoMapper extends BaseMapper<Fund, FundDto> {
 }
