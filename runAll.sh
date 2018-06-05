e8f747a5f00d2a8d0862b1703ad0817004d7a20b
Nico Peeters <nico.peeters@fundrequest.io>
2018-06-05 10:14:20 +0200

    Improve local setup -> admin pt4

--- runAll.sh
+++ runAll.sh
@@ -3,4 +3,5 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 mvn clean install -DskipTests=true
+java -Dspring.profiles.active="local" -jar ./tweb/target/tweb-0.0.1-SNAPSHOT.jar
 java -Dspring.profiles.active="local" -jar ./admin-web/target/admin-web-0.0.1-SNAPSHOT.jar
--- runPlatform.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=$(dirname "$0")
-cd "$BASEDIR"
-mvn clean install -DskipTests=true
-java -Dspring.profiles.active="local" -jar ./tweb/target/tweb-0.0.1-SNAPSHOT.jar
