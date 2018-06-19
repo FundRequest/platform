2bc6dd262379c437965ce0f818f76d9033bf75bc
davyvanroy <davy.van.roy@gmail.com>
2018-06-19 14:37:32 +0200

    add responsestatus to exception

--- tweb/src/main/java/io/fundreqest/platform/tweb/request/RequestRestController.java
+++ tweb/src/main/java/io/fundreqest/platform/tweb/request/RequestRestController.java
@@ -1,17 +1,13 @@
 package io.fundreqest.platform.tweb.request;
 
 import io.fundreqest.platform.tweb.request.dto.ClaimView;
-import io.fundrequest.core.infrastructure.exception.ResourceNotFoundException;
 import io.fundrequest.core.request.RequestService;
 import io.fundrequest.core.request.claim.dto.ClaimableResultDto;
 import io.fundrequest.core.request.domain.Platform;
 import io.fundrequest.core.request.view.RequestDto;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,9 +28,4 @@ public class RequestRestController {
         return new ClaimView(claimableResult.isClaimable(), claimableResult.getClaimableByPlatformUserName());
     }
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public void onResourceNotFound() {
-        // Intentionally blank
-    }
 }
