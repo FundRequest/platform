c1bae69a31001defe23404e2c8013680f02077cd
Nico Peeters <nico.peeters@fundrequest.io>
2018-07-12 10:41:28 +0200

    #293 - Fix test remarks -> stacktrace

--- core/src/main/java/io/fundrequest/core/message/dto/MessageDto.java
+++ core/src/main/java/io/fundrequest/core/message/dto/MessageDto.java
@@ -1,11 +1,15 @@
 package io.fundrequest.core.message.dto;
 
 import io.fundrequest.core.message.domain.MessageType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageDto {
     private Long id;
     private String name;
