31e67d7451ae7887ad27367b6ad0176fc6783651
Nico Peeters <nico.peeters@fundrequest.io>
2018-07-19 12:37:38 +0200

    #483 - Fix build

--- core/src/test/java/io/fundrequest/core/notification/infrastructure/NotificationRepositoryTest.java
+++ core/src/test/java/io/fundrequest/core/notification/infrastructure/NotificationRepositoryTest.java
@@ -4,11 +4,16 @@ import io.fundrequest.core.infrastructure.AbstractRepositoryTest;
 import io.fundrequest.core.notification.domain.Notification;
 import io.fundrequest.core.notification.domain.NotificationType;
 import io.fundrequest.core.notification.domain.RequestClaimedNotification;
+import io.fundrequest.core.request.domain.BlockchainEvent;
+import io.fundrequest.core.request.domain.Request;
+import io.fundrequest.core.request.domain.RequestMother;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,24 +21,22 @@ public class NotificationRepositoryTest extends AbstractRepositoryTest {
 
     @Autowired
     private NotificationRepository notificationRepository;
+    @Autowired
+    private EntityManager entityManager;
 
     @Test
     public void save() {
-        RequestClaimedNotification n = createNotification(1L, LocalDateTime.now(), 45L);
+        RequestClaimedNotification n = createNotification(LocalDateTime.now());
 
         notificationRepository.saveAndFlush(n);
 
         assertThat(n).isNotNull();
     }
 
-    private RequestClaimedNotification createNotification(long requestId, LocalDateTime date, long blockchainEventId) {
-        return new RequestClaimedNotification(NotificationType.REQUEST_CLAIMED, date, blockchainEventId, requestId, "davyvanroy");
-    }
-
     @Test
     public void findLast() {
         for (int i = 0; i < 20; i++) {
-            notificationRepository.save(createNotification((long) i, LocalDateTime.now().plusDays(i), 45L));
+            notificationRepository.save(createNotification(LocalDateTime.now().plusDays(i)));
         }
         notificationRepository.flush();
 
@@ -41,5 +44,13 @@ public class NotificationRepositoryTest extends AbstractRepositoryTest {
 
         assertThat(result).hasSize(10);
         assertThat(result).isSortedAccordingTo((o1, o2) -> o2.getDate().compareTo(o1.getDate()));
+    }
+
+    private RequestClaimedNotification createNotification(final LocalDateTime date) {
+        final Request request = RequestMother.fundRequestArea51().build();
+        final BlockchainEvent blockchainEvent = new BlockchainEvent(UUID.randomUUID().toString(), "1");
+        entityManager.persist(request);
+        entityManager.persist(blockchainEvent);
+        return new RequestClaimedNotification(NotificationType.REQUEST_CLAIMED, date, blockchainEvent.getId(), request.getId(), "davyvanroy");
     }
 }
