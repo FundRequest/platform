ef8ae088cadc71071796982ff00e2f412e0c8010
Nico Peeters <nico.peeters@fundrequest.io>
2018-07-27 09:56:28 +0200

    #487 - Add mock for local development

---
+++ core/src/main/java/io/fundrequest/core/config/IdentityApiConfig.java
@@ -0,0 +1,45 @@
+package io.fundrequest.core.config;
+
+import feign.Client;
+import feign.Contract;
+import feign.Feign;
+import feign.auth.BasicAuthRequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import io.fundrequest.core.identity.IdentityAPIClient;
+import io.fundrequest.core.identity.IdentityAPIClientMock;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(FeignClientsConfiguration.class)
+public class IdentityApiConfig {
+
+    @Bean
+    @ConditionalOnProperty(value = "io.fundrequest.mock.identity-api", havingValue = "false", matchIfMissing = true)
+    public IdentityAPIClient identityAPIClient(final Decoder decoder,
+                                               final Encoder encoder,
+                                               final Client client,
+                                               final Contract contract,
+                                               @Value("${io.fundrequest.identity.api.url}") final String url,
+                                               @Value("${io.fundrequest.identity.api.user.name}") final String username,
+                                               @Value("${io.fundrequest.identity.api.user.password}") final String password) {
+        return Feign.builder()
+                    .client(client)
+                    .encoder(encoder)
+                    .decoder(decoder)
+                    .contract(contract)
+                    .requestInterceptor(new BasicAuthRequestInterceptor(username, password))
+                    .target(IdentityAPIClient.class, url);
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "io.fundrequest.mock.identity-api", havingValue = "true")
+    public IdentityAPIClient mockIdentityAPIClient() {
+        return new IdentityAPIClientMock();
+    }
+}
--- core/src/main/java/io/fundrequest/core/identity/IdentityAPIClient.java
+++ core/src/main/java/io/fundrequest/core/identity/IdentityAPIClient.java
@@ -1,17 +1,11 @@
 package io.fundrequest.core.identity;
 
 import org.keycloak.representations.idm.UserRepresentation;
-import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@FeignClient(
-        name = "identity-api-client",
-        url = "${io.fundrequest.identity.api.url}",
-        configuration = IdentityAPIClientConfiguration.class
-)
 public interface IdentityAPIClient {
 
     @RequestMapping(value = "/users", params = {"identityProvider", "federatedUsername"}, method = GET)
