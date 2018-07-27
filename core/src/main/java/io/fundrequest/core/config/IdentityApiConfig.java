cc52d6bbf19a79a3604a3048ae9432e773c4df99
Nico Peeters <nico.peeters@fundrequest.io>
2018-07-27 16:07:29 +0200

    #487 - Fix mocking

--- core/src/main/java/io/fundrequest/core/config/IdentityApiConfig.java
+++ core/src/main/java/io/fundrequest/core/config/IdentityApiConfig.java
@@ -6,31 +6,42 @@ import feign.Feign;
 import feign.auth.BasicAuthRequestInterceptor;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
+import feign.httpclient.ApacheHttpClient;
 import io.fundrequest.core.identity.IdentityAPIClient;
 import io.fundrequest.core.identity.IdentityAPIClientMock;
+import org.apache.http.client.HttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.netflix.feign.FeignAutoConfiguration;
 import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import( {FeignClientsConfiguration.class, FeignAutoConfiguration.class})
+@Import( {FeignClientsConfiguration.class})
 public class IdentityApiConfig {
+
+    @Bean
+    @ConditionalOnProperty(value = "io.fundrequest.mock.identity-api", havingValue = "false", matchIfMissing = true)
+    public Client identityApiFeignClient(@Autowired(required = false) final HttpClient httpClient) {
+        if (httpClient != null) {
+            return new ApacheHttpClient(httpClient);
+        }
+        return new ApacheHttpClient();
+    }
 
     @Bean
     @ConditionalOnProperty(value = "io.fundrequest.mock.identity-api", havingValue = "false", matchIfMissing = true)
     public IdentityAPIClient identityAPIClient(final Decoder decoder,
                                                final Encoder encoder,
-                                               final Client client,
+                                               final Client identityApiFeignClient,
                                                final Contract contract,
                                                @Value("${io.fundrequest.identity.api.url}") final String url,
                                                @Value("${io.fundrequest.identity.api.user.name}") final String username,
                                                @Value("${io.fundrequest.identity.api.user.password}") final String password) {
         return Feign.builder()
-                    .client(client)
+                    .client(identityApiFeignClient)
                     .encoder(encoder)
                     .decoder(decoder)
                     .contract(contract)
