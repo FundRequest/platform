e38373dccc045ddab3ec5ca7fb6fafcf4714f2cd
davyvanroy <davy.van.roy@gmail.com>
2019-02-07 15:48:28 +0100

    handle exceptions

--- tweb/src/main/java/io/fundrequest/platform/tweb/infrastructure/thymeleaf/ProfilesExpressionObject.java
+++ tweb/src/main/java/io/fundrequest/platform/tweb/infrastructure/thymeleaf/ProfilesExpressionObject.java
@@ -3,6 +3,8 @@ package io.fundrequest.platform.tweb.infrastructure.thymeleaf;
 import io.fundrequest.platform.profile.profile.ProfileService;
 import io.fundrequest.platform.profile.profile.dto.UserProfile;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -11,6 +13,8 @@ import java.util.function.Function;
 @Component
 public class ProfilesExpressionObject {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProfilesExpressionObject.class);
+
     private final ProfileService profileService;
 
     public ProfilesExpressionObject(final ProfileService profileService) {
@@ -18,7 +22,13 @@ public class ProfilesExpressionObject {
     }
 
     public Optional<UserProfile> findByUserId(final String userId) {
-        return StringUtils.isBlank(userId) ? Optional.empty() : Optional.ofNullable(profileService.getUserProfile(() -> userId));
+        try {
+            return StringUtils.isBlank(userId) ? Optional.empty() : Optional.ofNullable(profileService.getUserProfile(() -> userId));
+        } catch (Exception e) {
+            LOGGER.error("Error getting profile for: \"" + userId + "\"", e);
+            return Optional.empty();
+        }
+
     }
 
     public Function<UserProfile, String> getName() {
