f4f50941b3d33ca2e32c6a1119df3c012707806f
alpacoder <alpacoder@gmail.com>
2021-03-01 20:36:36 +0100

    update keycloak config

--- keycloak/src/main/java/io/fundrequest/platform/keycloak/KeycloakConfig.java
+++ keycloak/src/main/java/io/fundrequest/platform/keycloak/KeycloakConfig.java
@@ -1,11 +1,14 @@
 package io.fundrequest.platform.keycloak;
 
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import static org.keycloak.OAuth2Constants.CLIENT_CREDENTIALS;
 
 @Configuration
 @Profile("!local")
@@ -22,11 +25,14 @@ public class KeycloakConfig {
 
     @Bean
     public RealmResource keycloak() {
-        final Keycloak keycloak = Keycloak.getInstance(
-                serverUrl,
-                realm,
-                clientId,
-                clientSecret);
+        Keycloak keycloak = KeycloakBuilder.builder()
+                                           .serverUrl(serverUrl)
+                                           .realm(realm)
+                                           .grantType(CLIENT_CREDENTIALS)
+                                           .clientId(clientId)
+                                           .clientSecret(clientSecret)
+                                           .build();
+
         return keycloak.realm(realm);
     }
 }
